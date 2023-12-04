531c5d655b1f26b6d726c95c8b2c8db733c0a81a
abduholiqovmuhammadzohir <abduholiqovmuhammadzohir@gmail.com>
2023-12-04 21:24:52 +0500

    homework

---
+++ index.js
@@ -0,0 +1,92 @@
+
+// 1 -  misol
+// let n = +prompt("son kiriting")
+// let sum = 0
+
+// for (let i = 1; i <= n; i++){
+//     if (i % 2 == 1) {
+//         sum += i
+//     }
+// }
+// console.log(sum);
+
+// 2 - misol
+
+// let n = +prompt("son kiriting")
+
+// let sum = 1
+
+// for (let i = 1; n >= i; i++){
+//     if ( i % 3 == 0) {
+//         sum *= i
+//     }
+// }
+// console.log(sum);
+
+// 3 - misol
+
+// let a = +prompt("son kiriting")
+// let b = +prompt("son kiriting")
+
+// let sum = 0
+
+// for (let i = a; b >= i; i++){
+//     if (i % 3 == 0) {
+//         console.log(i);
+//     }
+// }
+
+
+// 4 - misol
+
+// let a = +prompt("son kiriting")
+// let b = +prompt("son kiriting")
+
+// let sum = 0
+
+// for (let i = a; b >= i; i++){
+//     if (i % 5 == 0 && i % 7 == 0) {
+//         sum += i
+//     }
+// }
+
+// console.log(sum);
+
+// 5 - misol
+
+// let a = +prompt("son kiriting")
+// let b = +prompt("son kiriting")
+// let sum = 0;
+
+// for (let i = a; i <= b; i++){
+//     if (i % 2 == 1) {
+//         i **= 2
+//         sum += i
+//     }
+// }
+// console.log(sum);
+
+
+// 6 - misol
+
+// let a = +prompt("son kiriting")
+// let b = +prompt("son kiriting")
+// let sum = 0;
+
+// for (let i = a; i <= b; i++){
+//     if (i % 2 == 1) {
+//         i **= 2
+//         sum += i
+//     }
+// }
+// console.log(sum);
+
+// 7 - misol
+
+let a = +prompt("son kiriting")
+
+
+
+// 8 - misol
+// 9 - misol
+
